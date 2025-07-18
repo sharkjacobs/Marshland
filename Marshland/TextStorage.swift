fb53329ff30b50efcf651ab8b5772c8fb8f8a3a5
Graham Bing <grrraham@gmail.com>
2025-07-17 23:28:59 -0700

    Undo/redo replaceCharacters(in:with:)

--- Marshland/TextStorage.swift
+++ Marshland/TextStorage.swift
@@ -14,6 +14,8 @@ class TextStorage: NSTextStorage, @unchecked Sendable {
     private var editedCharactersRange: NSRange?
     weak var undoManager: UndoManager?
 
+    // MARK: - Initializers
+
     init(tendrilTree: TendrilTree = TendrilTree()) {
         self.tendrilTree = tendrilTree
         self.backingStorage = NSTextStorage()
@@ -28,7 +30,7 @@ class TextStorage: NSTextStorage, @unchecked Sendable {
         fatalError("init(pasteboardPropertyList:ofType:) has not been implemented")
     }
 
-    // MARK: - NSTextStorage Overrides
+    // MARK: - Accessors
 
     override var string: String {
         return backingStorage.string
@@ -44,29 +46,125 @@ class TextStorage: NSTextStorage, @unchecked Sendable {
         return backingStorage.attributes(at: location, effectiveRange: range)
     }
 
+    // MARK: - Insertion/Deletion
+
     override func replaceCharacters(in range: NSRange, with str: String) {
+        var lines = [IndentedLine]()
+        do {
+            let baseIndentation = try tendrilTree.indentation(at: range.location)
+            let wholeString = str.startIndex..<str.endIndex
+            str.enumerateSubstrings(in: wholeString, options: .byLines) { _, _, enclosingRange, _ in
+                let line = str[enclosingRange]
+                let tabCount = line.prefix(while: { $0 == "\t" }).count
+                let indentedLine = IndentedLine(
+                    content: String(line.dropFirst(tabCount)), indentation: tabCount + baseIndentation)
+                lines.append(indentedLine)
+            }
+        } catch {
+            fatalError("replaceCharacters on invalid range: \(error)")
+        }
+
+        replaceCharacters(in: range, with: lines)
+    }
+
+    func replaceCharacters(in range: NSRange, with lines: [IndentedLine]) {
         beginEditing()
 
-        var insertionLength = 0
-        var deletionLength = 0
         do {
-            try tendrilTree.delete(range: range) { _, delRange in
-                self.backingStorage.replaceCharacters(in: delRange, with: "")
-                deletionLength += delRange.length
-            }
-            try tendrilTree.insert(content: str, at: range.location) { insertion, delRange in
-                self.backingStorage.replaceCharacters(in: delRange, with: insertion)
-                insertionLength += insertion.utf16.count
-                deletionLength += delRange.length
-            }
+            let insertionLength = lines.reduce(0) { $0 + $1.content.utf16.count }
+            var deletionLength = 0
+
+            let deletedContent = try delete(range: range)
+            deletionLength = deletedContent.reduce(0) { $0 + $1.content.utf16.count }
+
+            insertLines(lines, to: range.location)
+
+            //            try tendrilTree.insert(content: str, at: range.location) { insertion, delRange in
+            //                insertionLength += insertion.utf16.count
+            //                deletionLength += delRange.length
+            //            }
+
+            edited([.editedCharacters], range: range, changeInLength: insertionLength - deletionLength)
+            editedCharactersRange = NSRange(location: range.location, length: insertionLength)
+            registerUndoForReplaceCharacters(in: editedCharactersRange!, with: deletedContent)
         } catch {
             print("Error updating tree: \(error)")
         }
 
-        edited([.editedCharacters], range: range, changeInLength: insertionLength - deletionLength)
-        editedCharactersRange = NSRange(location: range.location, length: insertionLength)
         endEditing()
+
     }
+
+    private func offsetIsAtNewlineBoundary(_ offset: Int) -> Bool {
+        guard offset > 0 else { return true }
+        do {
+            try _ = tendrilTree.rangeOfLine(at: offset)
+            return true
+        } catch {
+            return false
+        }
+    }
+
+    private func insertLines(_ lines: [IndentedLine], to offset: Int) {
+        guard lines.count > 0 else { return }
+
+        var insertionPoint = offset
+        for line in lines {
+            try? tendrilTree.insert(content: line.content, at: insertionPoint) { insertionContent, deletionRange in
+                if deletionRange.length > 0 {
+                    let deletedContent = self.backingStorage.attributedSubstring(from: deletionRange)
+                    print("What do I do with this: \(deletedContent)?")
+                    fatalError("huh?")
+                }
+
+                self.backingStorage.replaceCharacters(in: NSRange(location: insertionPoint, length: 0), with: insertionContent)
+                try? self.tendrilTree.setIndentation(to: line.indentation, at: insertionPoint)
+                insertionPoint += insertionContent.utf16.count
+            }
+        }
+
+        //        registerUndoForInsertion(range: NSRange(location: offset, length: insertionPoint))
+    }
+
+    // returns the deleted lines, for undo
+    private func delete(range: NSRange) throws -> [IndentedLine] {
+        guard range.length > 0 else { return [] }
+
+        var lines = [IndentedLine]()
+        tendrilTree.enumerateLines(in: range) { lineContent, lineRange, lineIndentation in
+            var content: any StringProtocol = lineContent
+            if range.location > lineRange.location {
+                content = content.dropFirst(range.location - lineRange.location)
+            }
+            if lineRange.upperBound > range.upperBound {
+                content = content.dropLast(lineRange.upperBound - range.upperBound)
+            }
+            if !content.isEmpty {
+                lines.append(IndentedLine(content: String(content), indentation: lineIndentation))
+            }
+        }
+
+        try tendrilTree.delete(range: range) { _, realRange in
+            self.backingStorage.replaceCharacters(in: realRange, with: "")
+
+            if realRange.length > range.length {
+                // We shouldn't have to know this about the internals of TendrilTree,
+                // but if realRange.length > range.length, it's because we deleted chars before \t,
+                // and they became the line prefix and were deleted
+                // so to undo we should reinsert those tabs (we're already setting indentation back)
+                let lastLine = IndentedLine(
+                    content: lines.last!.content + String(repeating: "\t", count: realRange.length - range.length),
+                    indentation: lines.last!.indentation
+                )
+                lines = lines.dropLast()
+                lines.append(lastLine)
+            }
+        }
+        //        registerUndoForDeletion(lines: lines, offset: range.location)
+        return lines
+    }
+
+    // MARK: - Attributes
 
     override open func invalidateAttributes(in range: NSRange) {
         let r = NSRange(location: range.location, length: min(range.length, backingStorage.length - range.location))
@@ -96,7 +194,8 @@ class TextStorage: NSTextStorage, @unchecked Sendable {
     override open var fixesAttributesLazily: Bool {
         return backingStorage.fixesAttributesLazily
     }
-    // MARK: - TendrilTree Indentation
+
+    // MARK: - Indentation
 
     func indent(_ ranges: [NSRange], updateTextView: @escaping () -> Void) throws {
         var undoRanges = [NSRange]()
@@ -105,8 +204,6 @@ class TextStorage: NSTextStorage, @unchecked Sendable {
             updateIndentationOfAttribute(for: range)
         }
         updateTextView()
-        //        updateIndentationOfTypingAttributes(in: textView)
-        //        field?.text = textStorage.fileString
         registerUndoForIndent(undoRanges) { updateTextView() }
     }
 
@@ -116,11 +213,6 @@ class TextStorage: NSTextStorage, @unchecked Sendable {
             try tendrilTree.outdent(range: range) { undoRanges.append(contentsOf: $0) }
             updateIndentationOfAttribute(for: range)
         }
-        //        updateIndentationOfTypingAttributes(in: textView)
-
-        // to force change
-        //        field?.text = textStorage.fileString
-        //        edited(.editedCharacters, range: NSRange(location: 0, length: 0), changeInLength: 0)
         updateTextView()
         registerUndoForOutdent(undoRanges) { updateTextView() }
     }
@@ -171,11 +263,25 @@ extension TextStorage {
     }
 }
 
-// MARK: - copying
+// MARK: - Copy/Paste
+
+struct IndentedLine: Codable {
+    let content: String
+    let indentation: Int
+
+    init(content: String, indentation: Int) {
+        self.content = content
+        assert(!content.hasPrefix("\t"))
+        self.indentation = indentation
+    }
+}
 
 extension TextStorage {
-    func copiedText(for range: NSRange) -> String? {
-        var result: String = ""
+    func copiedData(for range: NSRange) -> (String, [IndentedLine])? {
+        guard range.upperBound <= length else { return nil }
+
+        var str: String = ""
+        var lines: [IndentedLine] = []
 
         var linesLength: Int = 0
         var baseIndentation: Int = 0
@@ -185,23 +291,34 @@ extension TextStorage {
             if isFirstLine {
                 if lineRange.location < range.location {
                     let delta = range.location - lineRange.location
-                    result += lineContent.dropFirst(delta)
+                    str += lineContent.dropFirst(delta)
+                    lines.append(IndentedLine(content: str, indentation: lineIndentation))
                     linesLength -= delta
                 } else {
-                    result += lineContent
+                    str += lineContent
+                    lines.append(IndentedLine(content: lineContent, indentation: lineIndentation))
                 }
                 baseIndentation = lineIndentation
                 isFirstLine = false
             } else {
-                result += lineContent.withIndentation(lineIndentation - baseIndentation)
+                str += lineContent.withIndentation(lineIndentation - baseIndentation)
+                lines.append(IndentedLine(content: lineContent, indentation: lineIndentation))
             }
         }
         if linesLength > range.length {
             let delta = linesLength - range.length
-            result = String(result.dropLast(delta))
+            str = String(str.dropLast(delta))
+            if let indentation = lines.last?.indentation {
+                lines = lines.dropLast()
+                lines.append(IndentedLine(content: str, indentation: indentation))
+            }
         }
 
-        return result
+        return (str, lines)
+    }
+
+    func pasteLines(lines: [IndentedLine], at range: NSRange) {
+        replaceCharacters(in: range, with: lines)
     }
 }
 
@@ -236,5 +353,11 @@ extension TextStorage {
             try? target.indent(ranges) { updateTextView() }
         }
         undoManager?.setActionName("Outdent")
+    }
+
+    func registerUndoForReplaceCharacters(in range: NSRange, with lines: [IndentedLine]) {
+        undoManager?.registerUndo(withTarget: self) { target in
+            target.replaceCharacters(in: range, with: lines)
+        }
     }
 }
