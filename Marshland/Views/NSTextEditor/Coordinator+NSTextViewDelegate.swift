37adeef0b908cd16d1596d80e2c727c1380a8c72
Graham Bing <grrraham@gmail.com>
2025-09-18 15:49:39 -0700

    Refactor NSTextEditor files

--- Marshland/Views/NSTextEditor/Coordinator+NSTextViewDelegate.swift
+++ Marshland/Views/NSTextEditor/Coordinator+NSTextViewDelegate.swift
@@ -47,34 +47,3 @@ extension NSTextEditor.Coordinator: NSTextViewDelegate {
         return false
     }
 }
-
-extension NSTextEditor.Coordinator : NSTextContentStorageDelegate {
-    func textContentStorage(_ textContentStorage: NSTextContentStorage, textParagraphWith range: NSRange) -> NSTextParagraph? {
-        let originalText = textContentStorage.textStorage!.attributedSubstring(from: range)
-
-        func paragraphStyle(indentation: Int = 0) -> NSParagraphStyle {
-            let baseIndentation = 15
-            let indentSize = 20
-            let indent = CGFloat(baseIndentation + indentSize * indentation)
-
-            let paragraphStyle = NSMutableParagraphStyle()
-            paragraphStyle.firstLineHeadIndent = indent
-            paragraphStyle.headIndent = indent
-            return paragraphStyle
-        }
-
-        let displayAttributes: [NSAttributedString.Key: AnyObject] = [
-            .font: NSFont.systemFont(ofSize: 14),
-            .foregroundColor: NSColor.labelColor,
-            .paragraphStyle: paragraphStyle(indentation: (try? viewModel.indentation(at: range.location)) ?? 0)
-        ]
-        let textWithDisplayAttributes = NSMutableAttributedString(attributedString: originalText)
-        let rangeForDisplayAttributes = NSRange(location: 0, length: textWithDisplayAttributes.length)
-        textWithDisplayAttributes.addAttributes(displayAttributes, range: rangeForDisplayAttributes)
-        return NSTextParagraph(attributedString: textWithDisplayAttributes)
-    }
-}
-
-extension NSTextEditor.Coordinator : NSTextLayoutManagerDelegate {
-    
-}
--- Marshland/Views/NSTextEditor/NSTextEditor.swift
+++ Marshland/Views/NSTextEditor/NSTextEditor.swift
@@ -36,7 +36,7 @@ struct NSTextEditor: NSViewRepresentable {
         scrollView.borderType = .bezelBorder
         
         let layoutManager = textView.textContainer?.textLayoutManager
-        layoutManager?.delegate = context.coordinator
+        layoutManager?.delegate = context.coordinator as? any NSTextLayoutManagerDelegate
         let textContentStorage = layoutManager?.textContentManager as? NSTextContentStorage
         textContentStorage?.delegate = context.coordinator
 
@@ -146,83 +146,5 @@ struct NSTextEditor: NSViewRepresentable {
 
         func expand(_ range: NSRange, in textView: NSTextView) {
         }
-    }
-}
-
-// MARK: - IndentedTextview
-
-class MarshlandTextView: NSTextView {
-    override func viewDidMoveToWindow() {
-        super.viewDidMoveToWindow()
-        if let coordinator = self.delegate as? NSTextEditor.Coordinator {
-            coordinator.didAttachToWindow(textView: self)
-        }
-    }
-    /// Overrides the default copy behavior triggered by ⌘C or the Edit > Copy menu item.
-    /// This method is part of the NSResponder chain.
-    override func copy(_ sender: Any?) {
-        let range = self.selectedRange()
-
-        guard range.length > 0,
-            let coordinator = self.delegate as? NSTextEditor.Coordinator,
-            let chunk: PasteboardChunk = coordinator.viewModel.copiedData(for: range)
-        else {
-            super.copy(sender)
-            return
-        }
-
-        let pasteboard = NSPasteboard.general
-        pasteboard.clearContents()
-        pasteboard.setString(chunk.content, forType: .string)
-        if let data = try? JSONEncoder().encode(chunk) {
-            pasteboard.setData(data, forType: NSPasteboard.PasteboardType("com.gdb.marshlandchunk"))
-        }
-    }
-
-    override func paste(_ sender: Any?) {
-        let pb = NSPasteboard.general
-        if let data = pb.data(forType: NSPasteboard.PasteboardType("com.gdb.marshlandchunk")),
-            let chunk = try? JSONDecoder().decode(PasteboardChunk.self, from: data)
-        {
-            let insertRange = selectedRange()
-            self.undoManager?.beginUndoGrouping()
-            self.insertText(chunk.content as Any, replacementRange: insertRange)
-            let tempIs = chunk.indents.map { Indent(location: $0.location + insertRange.location, depth: $0.depth) }
-//            (self.delegate as? NSTextEditor.Coordinator)?.indent(tempIs, in: self)
-            self.undoManager?.endUndoGrouping()
-
-        } else {
-            super.paste(sender)
-        }
-
-    }
-
-    // enabling/disabling the "Copy" menu item
-    override func validateMenuItem(_ menuItem: NSMenuItem) -> Bool {
-        if menuItem.action == #selector(copy(_:)) {
-            return self.selectedRange().length > 0
-        }
-        if menuItem.action == #selector(paste(_:)) {
-            let pb = NSPasteboard.general
-            return pb.canReadItem(withDataConformingToTypes: ["com.gdb.marshlandchunk"])
-                || pb.canReadItem(withDataConformingToTypes: [NSPasteboard.PasteboardType.string.rawValue])
-        }
-        return super.validateMenuItem(menuItem)
-    }
-
-    // MARK: - Overscrolling
-    
-    func scrollViewDidResize(_ scrollView: NSScrollView) {
-        let offset = scrollView.bounds.height / 4 // half the window
-        textContainerInset = NSSize(width: 0, height: offset)
-        overscrollY = offset
-    }
-    
-    var overscrollY: CGFloat = 0
-
-    override var textContainerOrigin: NSPoint {
-        return super
-            .textContainerOrigin
-            .applying(.init(translationX: 0, y: -overscrollY))
     }
 }
